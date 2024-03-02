d8d08326ddf1b2106b5354ed6db5c6423472bd9d
monire ebadi <moni.ebadi@gmail.com>
2024-03-03 00:01:00 +0330

    chore: remove list and minor fix

--- main.js
+++ main.js
@@ -3,25 +3,7 @@ const form = document.getElementsByTagName("form")[0];
 
 const input  = document.getElementsByTagName('input')[0];
 
-const list  = document.getElementById('list');
-
-
 const searchLocationsUrl = (city) => `http://api.weatherapi.com/v1/search.json?key=90f57187a7384eaba4901536232803&q=${city}`
-
-input.addEventListener('focus', () => {
-  list.classList.add('active')
-})
-
-input.addEventListener('blur', () => {
-  list.classList.remove('active')
-})
-
-input.addEventListener('keyup', (event) => {
-  const query = event.target.value
-  if (query && query.length > 2) {
-    getLocations(query)
-  }
-})
 
 // add submit event listener to form
 form.addEventListener("submit", (event) => {
@@ -63,25 +45,12 @@ window.addEventListener("load", async () => {
   }
 });
 
-const getLocations = (query) => {
-  fetch(searchLocationsUrl(query))
-    .then((response) => response.json())
-    .then(data => {
-      for (let index = 0; index < data.length; index++) {
-        const element = data[index];
-
-        list.innerHTML += `<span>${element.name}</span>`
-      }
-    })
-}
-
 // get weather status used `weatherapi.com`
 const getWeatherStatus = (city)=>{
   const api = `https://api.weatherapi.com/v1/current.json?key=90f57187a7384eaba4901536232803&q=${city}&aqi=no`;
   fetch(api)
     .then((response) => response.json())
-    .then((response) => {
-      weather = response;
+    .then((weather) => {
       document.getElementById(
         "location"
       ).innerHTML = `${weather.location.name} / ${weather.location.country}`;
